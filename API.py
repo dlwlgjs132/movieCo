9f496be4ecdd4df93074cc06461686f85cd65b59
dlwlgjs132 <36119144+dlwlgjs132@users.noreply.github.com>
2020-10-26 21:35:13 +0900

    Update API.py

--- API.py
+++ API.py
@@ -4,8 +4,8 @@ from cgv import Cgv
 import ssl
 
 app = Flask(__name__)
-c = LotteCinema()
-g = Cgv()
+lotte = LotteCinema()
+cgv = Cgv()
 
 @app.route("/current-movie", methods=['GET'])
 def get_current_movie():
@@ -16,8 +16,8 @@ def get_current_movie():
     latitude = data['latitude']
     longitude = data['longitude']
 
-    lotte_data = c.get_current_movie(latitude, longitude)
-    cgv_data = g.get_current_movie(latitude, longitude)
+    lotte_data = lotte.get_current_movie(latitude, longitude)
+    cgv_data = cgv.get_current_movie(latitude, longitude)
 
     lotte_data['CurrentMovieData'].extend(cgv_data['CgvCurrentMovieData'])
     return jsonify(lotte_data)
@@ -25,17 +25,16 @@ def get_current_movie():
 @app.route("/trailer", methods=['GET'])
 def show_trailer():
     name_data = request.json
-    print("name_data : ",name_data)
     movie_name = name_data['movie_name']
     
-    return jsonify(c.show_trailer(movie_name))
+    return jsonify(lotte.show_trailer(movie_name))
 
 @app.route("/specific-time-movie/<latitude>/<longitude>/<start>/<end>", methods=['GET', 'POST'])
 def get_specific_time_movie(latitude,longitude,start, end=30):
     time_list = []
     if end == 30:
         time_list.append(start)
-        return jsonify(c.get_specific_time_movie(latitude, longitude,time_list))
+        return jsonify(lotte.get_specific_time_movie(latitude, longitude,time_list))
     
     time_list.append(start)
     time_list.append(end)
@@ -43,15 +42,15 @@ def get_specific_time_movie(latitude,longitude,start, end=30):
     lotte_data = {}
     cgv_data = {}
 
-    lotte_data = c.get_specific_time_movie(latitude, longitude, time_list)
-    cgv_data = g.get_specific_time_movie(latitude, longitude, time_list)
+    lotte_data = lotte.get_specific_time_movie(latitude, longitude, time_list)
+    cgv_data = cgv.get_specific_time_movie(latitude, longitude, time_list)
 
     lotte_data['SpecificTimeData'].extend(cgv_data['CgvSpecificTimeData'])
     return jsonify(lotte_data)
 
 @app.route("/movie-name/<movie_name>/<latitude>/<longitude>", methods=['GET', 'POST'])    
 def get_movie_name_theater(movie_name, latitude, longitude):
-    return jsonify(c.get_movie_name_theater(latitude, longitude, movie_name))
+    return jsonify(lotte.get_movie_name_theater(latitude, longitude, movie_name))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
